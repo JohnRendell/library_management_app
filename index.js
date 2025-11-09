fdd94341a4e85a601c788d84df8a251fa610cced
JohnRendell <catuiza.johnrendell@gmail.com>
2025-11-09 13:12:10 +0800

    Update env variable for mongodb to connect in vercel.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
     console.log("DB name: " + mongoose.connection.name)
   } catch (error) {

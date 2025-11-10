7efef84dff93fac5dad7da08862569673e278b6f
gelni0221 <antipolonigel@gmail.com>
2025-11-10 12:57:25 +0800

    objectid explain

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,7 @@ const userModelSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true }, 
     // STORES REFERENCE NG BOOK SA MAY BOOK MODEL SCHEMA NAKA COMMENT MUNA PARA MA CHECK NYO
+    // HINDI UNG bookID kinukuha nito bale ung mismong ObjectId ng isang object sa schema ung kinukuha nya ung katulad nito ObjectId("6563f1a1a0b5c3e7a9d12345")
     // borrowedBooks: [{ type: Schema.Types.ObjectId, ref: 'Books' }]
 });
 

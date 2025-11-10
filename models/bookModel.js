e65c6751a83c5a5079d926db5eeb229d655db2c8
gelni0221 <antipolonigel@gmail.com>
2025-11-10 12:45:12 +0800

    book model and user model changes

---
+++ models/bookModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const database = require("./database_config")
+
+
+const bookModelSchema = new mongoose.Schema({
+    bookID: { type: Number, required: true },
+    title: { type: String, required: true },
+    author: { type: String, required: true }, 
+    genre: { type: String, required: true }, 
+    publisher: { type: String, required: true }, 
+    // "YYYY-MM-DD"
+    publication_date: { type: String, required: true},
+    availability: { type: String, required: true}
+});
+
+
+module.exports = database.model("book", bookModelSchema)
--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const database = require("./database_config")
+const bookModelSchema = require("../models/bookModel");
 
 const userModelSchema = new mongoose.Schema({
     userID: { type: Number, required: true },
     username: { type: String, required: true },
     password: { type: String, required: true }, 
+    // STORES REFERENCE NG BOOK SA MAY BOOK MODEL SCHEMA NAKA COMMENT MUNA PARA MA CHECK NYO
+    // borrowedBooks: [{ type: Schema.Types.ObjectId, ref: 'Books' }]
 });
+
+const Books = mongoose.model('Books', bookModelSchema);
 
 module.exports = database.model("account", userModelSchema)
